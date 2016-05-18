5756a2fe780937254d403c725916cd64e4e41ab6
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-05-18 20:33:10 -0300

    Seccion se crea con sufijo.

--- markdown-a-html/src/main/java/ar/edu/untref/aydoo/Seccion.java
+++ markdown-a-html/src/main/java/ar/edu/untref/aydoo/Seccion.java
@@ -3,13 +3,19 @@ package ar.edu.untref.aydoo;
 public class Seccion {
 	
 	private String prefijo;
+	private String sufijo;
 	
 	public Seccion(){
 		this.prefijo = "<section>";
+		this.sufijo = "</section>";
 	}
 
 	public String getPrefijo() {
 		return this.prefijo;
+	}
+
+	public String getSufijo() {
+		return this.sufijo;
 	}
 
 }
--- markdown-a-html/src/test/java/ar/edu/untref/aydoo/SeccionTest.java
+++ markdown-a-html/src/test/java/ar/edu/untref/aydoo/SeccionTest.java
@@ -4,15 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SeccionTest {
-	
+
 	@Test
 	public void seccionSeCreaConPrefijo(){
-		
+
 		Seccion seccion = new Seccion();
-		
+
 		String prefijo = seccion.getPrefijo();
-		
+
 		Assert.assertEquals("<section>", prefijo);
+	}
+
+	@Test
+	public void seccionSeCreaConSufijo(){
+
+		Seccion seccion = new Seccion();
+
+		String sufijo = seccion.getSufijo();
+
+		Assert.assertEquals("</section>", sufijo);
 	}
 
 }
