133fff4f92a220313e1f92656b133e8d5ba23a7c
ferGainey <fernandogainey@hotmail.com>
2016-05-19 00:07:02 -0300

    se agrego un lector de Markdown, que lee el archivo y devuelve un string con el contenido

---
+++ markdown-a-html/src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
@@ -0,0 +1,30 @@
+package ar.edu.untref.aydoo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LectorDeMarkdown {
+
+	public String leerMarkDown(String direccionDelArchivo) throws IOException{
+		File archivo = new File (direccionDelArchivo);
+		FileReader fr = new FileReader (archivo);
+		BufferedReader br = new BufferedReader(fr);
+		boolean seguirLeyendo = true;
+		String textoADevolver = "";
+		while(seguirLeyendo == true){
+		String linea = br.readLine();
+		if(linea == null){
+			seguirLeyendo = false;
+		}
+		else{
+			textoADevolver += linea;
+			textoADevolver += "\n";
+		}
+		}
+		br.close();
+		return textoADevolver;
+	}
+	
+}
