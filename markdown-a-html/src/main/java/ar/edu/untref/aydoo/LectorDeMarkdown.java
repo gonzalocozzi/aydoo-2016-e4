133c393d04068ebd0b685c3832f3957d9f452b21
juliani2 <julianezequielmoreno@gmail.com>
2016-05-19 14:12:10 -0300

    nuevo branch implementacion visitor

--- markdown-a-html/src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
+++ markdown-a-html/src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LectorDeMarkdown {
-
+	
 	public List<String> leerMarkDown(String direccionDelArchivo) throws IOException{
 		File archivo = new File (direccionDelArchivo);
 		FileReader fr = new FileReader (archivo);
