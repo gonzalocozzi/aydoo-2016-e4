65413a3931ce73c348063a69a81e2fbbb9d33214
ferGainey <fernandogainey@hotmail.com>
2016-05-19 01:40:42 -0300

    se devuelve una Lista en vez de un String en el metodo LectorDeMarkdown

--- markdown-a-html/src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
+++ markdown-a-html/src/main/java/ar/edu/untref/aydoo/LectorDeMarkdown.java
@@ -4,27 +4,28 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class LectorDeMarkdown {
 
-	public String leerMarkDown(String direccionDelArchivo) throws IOException{
+	public List<String> leerMarkDown(String direccionDelArchivo) throws IOException{
 		File archivo = new File (direccionDelArchivo);
 		FileReader fr = new FileReader (archivo);
 		BufferedReader br = new BufferedReader(fr);
 		boolean seguirLeyendo = true;
-		String textoADevolver = "";
+		List<String> listaADevolver = new LinkedList<String>();
 		while(seguirLeyendo == true){
 			String linea = br.readLine();
 			if(linea == null){
 				seguirLeyendo = false;
 			}
 			else{
-				textoADevolver += linea;
-				textoADevolver += "\n";
+				listaADevolver.add(linea);
 			}
 		}
 		br.close();
-		return textoADevolver;
+		return listaADevolver;
 	}
 
 }
