9978683ad729aa8f8f62ad568fd272bc2c600cf4
ferGainey <fernandogainey@hotmail.com>
2016-05-23 01:58:11 -0300

    si hay una seccion abierta las etiquetas que le siguen se agregan a la seccion

---
+++ src/test/java/ar/edu/untref/aydoo/OrganizadorDeEtiquetasTest.java
@@ -0,0 +1,25 @@
+package ar.edu.untref.aydoo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OrganizadorDeEtiquetasTest {
+
+	@Test
+	public void siHayUnaSeccionAbiertaLasEtiquetasQueLeSiguenSeAgreganALaSeccion(){
+		OrganizadorDeEtiquetas miOrganizador = new OrganizadorDeEtiquetas();
+		List<EtiquetaHTML> listaDeEtiquetas = new LinkedList<EtiquetaHTML>();
+		List<EtiquetaHTML> listaOrganizada = new LinkedList<EtiquetaHTML>();
+		listaDeEtiquetas.add(new Seccion());
+		listaDeEtiquetas.add(new Titulo());
+		listaDeEtiquetas.add(new Subtitulo());
+		listaOrganizada = miOrganizador.organizarEtiquetasHTML(listaDeEtiquetas);
+		Seccion seccion = (Seccion) listaOrganizada.get(0);
+		Assert.assertEquals(Seccion.class, listaOrganizada.get(0).getClass());
+		Assert.assertEquals(Titulo.class, seccion.getListaDeElementos().get(0).getClass());
+		Assert.assertEquals(Subtitulo.class, seccion.getListaDeElementos().get(1).getClass());
+	}
+}
