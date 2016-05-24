ebeb23b7e7bb26359c78db532d8e8ce26b293dfe
ferGainey <fernandogainey@hotmail.com>
2016-05-23 22:32:14 -0300

    los items van dentro de una lista de items y la lista dentro de su seccion correspondiente si hay mas de una seccion

--- src/test/java/ar/edu/untref/aydoo/OrganizadorDeEtiquetasTest.java
+++ src/test/java/ar/edu/untref/aydoo/OrganizadorDeEtiquetasTest.java
@@ -96,4 +96,38 @@ public class OrganizadorDeEtiquetasTest {
 		Assert.assertEquals(ItemDeLista.class, ((((ListaSinOrden) ((Seccion) listaOrganizada.get(1)).getListaDeElementos().get(0)).getColeccionDeItems().get(1).getClass())));
 		Assert.assertEquals("tercer item", ((((ListaSinOrden) ((Seccion) listaOrganizada.get(1)).getListaDeElementos().get(0)).getColeccionDeItems().get(1).getTexto())));
 	}
+	
+	@Test
+	public void losItemsVanDentroDeUnaListaDeItemsYLaListaDentroDeSuSeccionCorrespondienteSiHayMasDeUnaSeccion(){
+		OrganizadorDeEtiquetas miOrganizador = new OrganizadorDeEtiquetas();
+		List<EtiquetaHTML> listaDeEtiquetas = new LinkedList<EtiquetaHTML>();
+		List<EtiquetaHTML> listaOrganizada = new LinkedList<EtiquetaHTML>();
+		ItemDeLista item1 = new ItemDeLista();
+		ItemDeLista item2 = new ItemDeLista();		
+		ItemDeLista item3 = new ItemDeLista();
+		TextoSinFormato texto = new TextoSinFormato();
+		Seccion seccion1 = new Seccion();
+		Seccion seccion2 = new Seccion();
+		item1.setTexto("primer item");
+		item2.setTexto("segundo item");
+		texto.setTexto("texto normal");
+		item3.setTexto("tercer item");
+		listaDeEtiquetas.add(seccion1);
+		listaDeEtiquetas.add(item1);//este se tiene que guardar fuera de la seccion, porque esta antes
+		listaDeEtiquetas.add(item2);
+		listaDeEtiquetas.add(seccion2);
+		listaDeEtiquetas.add(texto);
+		listaDeEtiquetas.add(item3);
+		listaOrganizada = miOrganizador.organizarEtiquetasHTML(listaDeEtiquetas);
+		
+		Assert.assertEquals(ItemDeLista.class, ((((ListaSinOrden) ((Seccion) listaOrganizada.get(0)).getListaDeElementos().get(0)).getColeccionDeItems().get(0).getClass())));
+		Assert.assertEquals("primer item", ((((ListaSinOrden) ((Seccion) listaOrganizada.get(0)).getListaDeElementos().get(0)).getColeccionDeItems().get(0).getTexto())));
+		Assert.assertEquals(ItemDeLista.class, ((((ListaSinOrden) ((Seccion) listaOrganizada.get(0)).getListaDeElementos().get(0)).getColeccionDeItems().get(1).getClass())));
+		Assert.assertEquals("segundo item", ((((ListaSinOrden) ((Seccion) listaOrganizada.get(0)).getListaDeElementos().get(0)).getColeccionDeItems().get(1).getTexto())));
+		
+		Assert.assertEquals(TextoSinFormato.class, (((Seccion) listaOrganizada.get(1)).getListaDeElementos().get(0).getClass()));
+		Assert.assertEquals("texto normal", (((Seccion) listaOrganizada.get(1)).getListaDeElementos().get(0).getTexto()));
+		Assert.assertEquals(ItemDeLista.class, ((((ListaSinOrden) ((Seccion) listaOrganizada.get(1)).getListaDeElementos().get(1)).getColeccionDeItems().get(0).getClass())));
+		Assert.assertEquals("tercer item", ((((ListaSinOrden) ((Seccion) listaOrganizada.get(1)).getListaDeElementos().get(1)).getColeccionDeItems().get(0).getTexto())));
+	}
 }
