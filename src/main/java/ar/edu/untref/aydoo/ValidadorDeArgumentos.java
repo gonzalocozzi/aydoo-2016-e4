2b027e93405f9f4741836b09620a2c1708bd4fe6
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-05-24 10:32:43 -0300

    Refactorizado validador de argumentos para mejorar claridad del codigo.

--- src/main/java/ar/edu/untref/aydoo/ValidadorDeArgumentos.java
+++ src/main/java/ar/edu/untref/aydoo/ValidadorDeArgumentos.java
@@ -10,19 +10,23 @@ public class ValidadorDeArgumentos {
 		this.argumentoEsValido = false;
 	}
 
-	public void validarListaDeArgumentos(List<String> listaDeArgumentos) {				
-
-		String stringDeArgumentos = listaDeArgumentos.toString();
+	public void validarListaDeArgumentos(List<String> listaDeArgumentos) {
 		
-		if (listaDeArgumentos.size() == 0 || listaDeArgumentos.size() == 1 && listaDeArgumentos.get(0).contains("--")) {			
+		String stringDeArgumentos = listaDeArgumentos.toString();
+		Boolean sinNombreDelArchivoDeEntrada = listaDeArgumentos.size() == 0 || listaDeArgumentos.size() == 1 && listaDeArgumentos.get(0).contains("--");
+		Boolean argumentoInvalido = listaDeArgumentos.size() == 2 && !stringDeArgumentos.contains("--mode=default") && !stringDeArgumentos.contains("--mode=no-output") && !stringDeArgumentos.contains("--output=");
+		Boolean soloArgumentosDeConfiguracion = listaDeArgumentos.size() == 2 && listaDeArgumentos.get(0).contains("--") && listaDeArgumentos.get(1).contains("--");
+	
+		
+		if (sinNombreDelArchivoDeEntrada) {			
 
 			throw new SinNombreDelArchivoDeEntradaException();
 
-		} else if (listaDeArgumentos.size() == 2 && !stringDeArgumentos.contains("--mode=default") && !stringDeArgumentos.contains("--mode=no-output") && !stringDeArgumentos.contains("--output=")) {
+		} else if (argumentoInvalido) {
 
 			throw new ArgumentoInvalidoException();
 
-		} else if (listaDeArgumentos.size() == 2 && listaDeArgumentos.get(0).contains("--") && listaDeArgumentos.get(1).contains("--")) {
+		} else if (soloArgumentosDeConfiguracion) {
 
 			throw new SinNombreDelArchivoDeEntradaException();
 
