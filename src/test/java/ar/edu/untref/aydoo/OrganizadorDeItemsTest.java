2523267487367cfb315eac5814db4daed516d4ba
ferGainey <fernandogainey@hotmail.com>
2016-05-26 03:45:35 -0300

    se agregan varios items y todos conservan su texto

--- src/test/java/ar/edu/untref/aydoo/OrganizadorDeItemsTest.java
+++ src/test/java/ar/edu/untref/aydoo/OrganizadorDeItemsTest.java
@@ -60,4 +60,35 @@ public class OrganizadorDeItemsTest {
 		Assert.assertEquals(listaEsperada.get(2).getClass(), listaOrganizadaObtenida.get(2).getClass());
 	}
 	
+	@Test
+	public void seAgreganVariosItemsYTodosConservanSuTexto(){
+		OrganizadorDeItems organizadorDeItems = new OrganizadorDeItems();
+		List<EtiquetaHTML> listaDeEtiquetas = new LinkedList<EtiquetaHTML>();
+		List<EtiquetaHTML> listaOrganizadaObtenida = new LinkedList<EtiquetaHTML>();
+		List<String> listaDeTextosGenerados = new LinkedList<String>();
+		ItemDeLista item1 = new ItemDeLista();
+		item1.setTexto("item 1");
+		ItemDeLista item2 = new ItemDeLista();
+		item2.setTexto("item 2");
+		ItemDeLista item3 = new ItemDeLista();
+		item3.setTexto("item 3");
+		listaDeEtiquetas.add(item1);
+		listaDeEtiquetas.add(item2);
+		listaDeEtiquetas.add(item3);
+		listaOrganizadaObtenida = organizadorDeItems.organizar(listaDeEtiquetas);
+		String textoDelItem1 = ((ListaSinOrden) listaOrganizadaObtenida.get(0)).getColeccionDeItems().get(0).getTexto();
+		String textoDelItem2 = ((ListaSinOrden) listaOrganizadaObtenida.get(0)).getColeccionDeItems().get(1).getTexto();
+		String textoDelItem3 = ((ListaSinOrden) listaOrganizadaObtenida.get(0)).getColeccionDeItems().get(2).getTexto();
+		listaDeTextosGenerados.add(textoDelItem1);
+		listaDeTextosGenerados.add(textoDelItem2);
+		listaDeTextosGenerados.add(textoDelItem3);
+		
+		List<String> listaDeTextosEsperada = new LinkedList<String>();
+		listaDeTextosEsperada.add("item 1");
+		listaDeTextosEsperada.add("item 2");
+		listaDeTextosEsperada.add("item 3");
+		
+		Assert.assertEquals(listaDeTextosEsperada, listaDeTextosGenerados);
+		
+	}
 }
